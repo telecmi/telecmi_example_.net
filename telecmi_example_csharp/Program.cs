b89aff4efbbc18d743293d182eb032a802d10fec
prasathsekar <prasath@telecmi.com>
2020-11-30 18:39:17 +0530

    files added

---
+++ telecmi_example_csharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace telecmi_example_csharp
+{
+    class Program
+    {
+        static HttpListener _httpListener = new HttpListener();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting server...");
+            _httpListener.Prefixes.Add("http://localhost:5000/"); // add prefix "http://localhost:5000/"
+            _httpListener.Start(); // start server (Run application as Administrator!)
+            Console.WriteLine("Server started.");
+            Thread _responseThread = new Thread(ResponseThread);
+            _responseThread.Start(); // start the response thread
+        }
+        static void ResponseThread()
+        {
+            while (true)
+            {
+                HttpListenerContext context = _httpListener.GetContext(); // get a context             
+                var request = context.Request;
+                string text;
+                using (var reader = new StreamReader(request.InputStream,
+                                                     request.ContentEncoding))
+                {
+                    text = reader.ReadToEnd();
+                }
+                Console.WriteLine("POST Data from the request.");
+                Console.WriteLine(text);
+                context.Response.Close(); // close the connection
+            }
+        }
+    }
+}
